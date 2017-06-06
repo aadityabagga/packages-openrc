b6b3310645ab1121234f2e5da950e532e442d4a7
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-07 00:33:22 +0530

    fixing some mistakes

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -6,6 +6,6 @@ PKGS=$(cat /tmp/packages_changed.txt)
 for pkg in ${PKGS}; do
 	echo "building $pkg"
 	cd "${pkg}"
-	mkchrootpkg "$HOME/manjaro-chroot"
+	mkchrootpkg -r "$HOME/manjaro-chroot"
 	cd ..
 done
