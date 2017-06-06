5a50c3eace1dd4b90c06e13247f9276f73c84803
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-06 17:46:29 +0530

    trying to correct no packages found

--- scripts/build_packages.sh
+++ scripts/build_packages.sh
@@ -4,6 +4,7 @@
 PKGS=$(cat /tmp/packages_changed.txt)
 
 for pkg in ${PKGS}; do
+	echo "building $pkg"
 	cd "${pkg}"
 	makechrootpkg
 	cd ..
--- scripts/changed_packages.sh
+++ scripts/changed_packages.sh
@@ -4,4 +4,5 @@
 # adapted from https://github.com/voidlinux/void-packages/blob/master/common/travis/changed_templates.sh
 
 /bin/echo -e '\x1b[32mChanged packages:\x1b[0m \n'
-git diff --name-only FETCH_HEAD...HEAD | grep "PKGBUILD$" | cut -d/ -f 1 | tee /tmp/packages_changed.txt
+#git diff --name-only FETCH_HEAD...HEAD | grep "PKGBUILD$" | cut -d/ -f 1 | tee /tmp/packages_changed.txt
+git diff --name-only | grep "PKGBUILD$" | cut -d/ -f 1 | tee /tmp/packages_changed.txt
