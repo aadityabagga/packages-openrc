eadef4a28698b8de3dfda5c7d8ef3e8f237314f6
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-10 23:06:50 +0530

    [travis] forgot to update package for testing

--- scripts/setup_build_environment.sh
+++ scripts/setup_build_environment.sh
@@ -8,6 +8,9 @@ cd "$HOME"
 git clone https://github.com/aadityabagga/manjaro-bootstrap.git
 sudo manjaro-bootstrap/arch-bootstrap.sh -d "$HOME/pkg_download" manjaro-chroot
 
+# create user for building package
+sudo chroot manjaro-chroot /bin/bash -c "useradd builder"
+
 # setup pacman mirrorlist
 # TODO
 
