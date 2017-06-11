41b0bfb0093ac055e791759823c7898b2ecb50cf
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-11 20:55:19 +0530

    fix manjaro-bootstrap usage + unmount mounted dirs

--- scripts/setup_build_environment.sh
+++ scripts/setup_build_environment.sh
@@ -3,28 +3,18 @@
 
 ORIGINAL_DIR="$(pwd)"
 CHROOT_DIR_NAME="manjaro-chroot"
-CHROOT_DIR_LOC="${HOME}/manjaro-chroot"
+CHROOT_DIR="${HOME}/manjaro-chroot"
 CHROOT_BUILD_DIR_NAME="build"
-CHROOT_BUILD_DIR_LOC="${CHROOT_DIR_LOC}/${CHROOT_BUILD_DIR_NAME}"
+CHROOT_BUILD_DIR="${CHROOT_DIR}/${CHROOT_BUILD_DIR_NAME}"
 
 # download script to setup chroot for building
 cd "$HOME"
 git clone https://github.com/aadityabagga/manjaro-bootstrap.git
-sudo manjaro-bootstrap/arch-bootstrap.sh -d "$HOME/pkg_download" manjaro-chroot
+cd manjaro-bootstrap
+sudo arch-bootstrap.sh -d "$HOME/pkg_download" "${CHROOT_DIR}"
+cd -
 
 # copy things forward to a new directory within the chroot
-sudo mkdir "${CHROOT_BUILD_DIR_LOC}"
-sudo chmod -R 777 "${CHROOT_BUILD_DIR_LOC}"
-cp -a "${ORIGINAL_DIR}" "${CHROOT_BUILD_DIR_LOC}"
-
-# create user for building package
-#sudo chroot manjaro-chroot /bin/bash -c "useradd builder"
-
-# setup pacman mirrorlist
-# TODO
-
-# initialize pacman
-# TODO
-
-# sync pacman
-#sudo pacman -Syu
+sudo mkdir "${CHROOT_BUILD_DIR}"
+sudo chmod -R 777 "${CHROOT_BUILD_DIR}"
+cp -a "${ORIGINAL_DIR}" "${CHROOT_BUILD_DIR}"
