b47332fa5bcdbc8faa383837c4c4ea89462f1003
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-12 18:13:27 +0530

    try to use bsdtar to reduce log output

--- .travis/scripts/install_dependencies.sh
+++ .travis/scripts/install_dependencies.sh
@@ -3,4 +3,9 @@
 # https://docs.travis-ci.com/user/installing-dependencies/
 
 sudo apt-get -qq update
-sudo apt-get install -y libarchive-dev docbook2x
+#sudo apt-get install -y libarchive-dev docbook2x
+sudo apt-get install -y bsdtar
+
+# make bsdtar the default
+sudo mv /bin/tar /bin/gnutar
+sudo mv /usr/bin/bsdtar /usr/bin/tar
