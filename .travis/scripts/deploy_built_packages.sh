60f9b80928ec0e159d8b005b5b85519781172519
Aaditya Bagga <aaditya_gnulinux@zoho.com>
2017-06-13 15:58:05 +0530

    testing travis variables

--- .travis/scripts/deploy_built_packages.sh
+++ .travis/scripts/deploy_built_packages.sh
@@ -4,6 +4,8 @@
 
 PKG_LOCATION="$HOME/manjaro-chroot/var/cache/manjaro-tools/pkg/unstable/x86_64/"
 SFREPO="frs.sourceforge.net:/home/frs/project/mefiles/Manjaro/test/packages-openrc/x86_64/"
+#SFUSER="aaditya1234"
+SFUSER=$1
 
 if [ ! -f "${HOME}/.ssh/id_rsa" ]; then
 	# cant deploy without key
